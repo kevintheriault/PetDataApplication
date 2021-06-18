31a72c5001b444ae389eff9434a802a4726a526c
kevintheriault <kevin.theriault@live.com>
2021-06-18 17:47:32 -0400

    added properties to set up H2 database.  Created schema.sql for database.

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE student (
+                         id INT AUTO_INCREMENT PRIMARY KEY,
+                         pet_name VARCHAR(30),
+                         owner_last_name VARCHAR(30),
+                         pet_kind VARCHAR(30),
+                         pet_gender VARCHAR(30),
+                         vaccinated BOOLEAN
+);
