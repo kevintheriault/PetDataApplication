6e1ad0191607bcfe564c3d4f09b293079f349c38
kevintheriault <kevin.theriault@live.com>
2021-06-18 18:53:57 -0400

    fixed logging issue. added all routing for pages.

--- src/main/java/sheridan/theriake/assignment2/controller/PetDataController.java
+++ src/main/java/sheridan/theriake/assignment2/controller/PetDataController.java
@@ -14,4 +14,36 @@ public class PetDataController {
         log.trace("index.html served with index()");
         return "Index";
     }
+
+    @GetMapping("/ListPets")
+    public String listPets(){
+        log.trace("listPets() called");
+        return "ListPets";
+    }
+
+    @GetMapping("/AddPet")
+    public String addPet(){
+        log.trace("addPet() called");
+        return "AddPet";
+    }
+
+    @GetMapping("/DeletePet")
+    public String deletePet(){
+        log.trace("deletePet() called");
+        return "DeletePet";
+    }
+
+    @GetMapping("/DetailPet")
+    public String detailPet(){
+        log.trace("detailPet() called");
+        return "DetailPet";
+    }
+
+    @GetMapping("/EditPet")
+    public String editPet(){
+        log.trace("editPet() called");
+        return "EditPet";
+    }
+
+
 }
