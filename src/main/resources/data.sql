21115a7da4c4c56033f8cf00d346cb97057f5b66
kevintheriault <kevin.theriault@live.com>
2021-06-18 18:09:17 -0400

    setup the database files with some pre-inputted pet data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO pet
+(pet_name, owner_last_name, pet_kind, pet_gender, vaccinated)
+VALUES
+('Louis','Theriault','Cat','M',true),
+('Lily','Theriault','Cat','F',false),
+('Cosmo','Theriault','Rabbit','F',false);
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE student (
+CREATE TABLE pet (
                          id INT AUTO_INCREMENT PRIMARY KEY,
                          pet_name VARCHAR(30),
                          owner_last_name VARCHAR(30),
